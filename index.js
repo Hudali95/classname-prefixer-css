1bc0329e0b21ffe71e2b2271aa776cf37aa87cc9
mahesh.hudali <mahesh.hudali@mpokket.com>
2024-10-06 22:49:54 +0530

    added support for file exclude and the debugg

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-
 const csstree = require('css-tree');
 const path = require('path');
 const fs = require('fs');
 
+const outputPath1 = path.resolve(__dirname, '..', 'css-classnames.txt'); // Adjusted path to point to the root folder
+
 function loader(source) {
     const options = this.getOptions();
     const prefix = options.prefix || 'app'; // Default to 'app' if no prefix is provided
+
+    // Get the filename from the resource path
+    const fileName = path.basename(this.resourcePath);
+
+    // Check if the filename is 'imported-styles.scss' or any other file to ignore
+    if (options.exclude.includes(fileName)) {
+        return source; // Return the original source without any modification
+    }
 
     // Parse the CSS into an AST
     const ast = csstree.parse(source);
@@ -14,6 +23,8 @@ function loader(source) {
     csstree.walk(ast, {
         visit: 'ClassSelector',
         enter(node) {
+            if (options.enableDebugging) fs.appendFileSync(outputPath1, `${node.name}"\n\n`);
+
             // Prefix the class name
             node.name = `${prefix}-${node.name}`;
         }
@@ -21,7 +32,6 @@ function loader(source) {
 
     // Generate the modified CSS from the AST
     const modifiedCSS = csstree.generate(ast);
-
 
     // Return the modified CSS
     return modifiedCSS;
